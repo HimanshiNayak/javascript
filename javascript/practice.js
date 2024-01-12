58e3120a68e34d8b422532fbc1cbd55fa39436fa
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2024-01-12 22:25:39 +0530

    Add files via upload

---
+++ javascript/practice.js
@@ -0,0 +1,26 @@
+//create object for amazon product
+const product={
+    name :"Parker Joter Ball Pen",
+    rattings:7002,
+    isdeal:true,
+    price:270,
+    offer:5,
+};
+console.log(product);
+console.log(typeof(product)); //obj
+
+// online profile
+
+const profile={
+    name:"Harry",
+    posts:195,
+    followers:596000,
+    following:4,
+    isfollow:False,
+    ismessage:False,
+    job:"Eunterpreneur",
+    worksat:"Apna college",
+    extra:"ex-microsoft ,DRDO",
+    about:"To educate someone is the highest privilege",
+};
+console.log(profile);
