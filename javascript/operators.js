55716c68c6217b589da09f06f78c943b5f3b7969
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2024-01-14 23:09:20 +0530

    Add files via upload

---
+++ javascript/operators.js
@@ -0,0 +1,93 @@
+// + -* / % ++ -- :arithematic operators
+let a=5;
+let b=2;
+console.log("a:",a,"&b:",b);
+console.log("a+b:",a+b);
+console.log("a-b:",a-b);
+console.log("a*b:",a*b);
+console.log("a/b:",a/b);
+console.log("a%b:",a%b);
+//unary
+console.log("a++:",a++);
+console.log("b++:",b++);
+console.log("++b:",++b);
+console.log("a--",a--);
+console.log("--a",--a)
+
+//assingement operators:+= , -=,*=,/=,%=
+ a**=a;
+console.log("a**=a" ,a) //element raised to the power of itself
+
+//comparsion operators: boolean answer
+// > ,<a,=,(== ,!= ): checks only the data not the type
+// === and !== checks th type as well
+
+let c="6";
+let d=6;
+console.log("c==d",c==d) //returns true
+console.log("c===d",c===d) //false a sthey are not equal in theie type
+
+//logical operators:&& || !: answer in boolean
+
+
+//practice:
+//1.user input and check if mul of  or not
+
+let n=prompt("Enter a number");
+if(n%5===0){
+    console.log(n,"is  a multiple of 5");
+}
+else{
+    console.log("Not a multiple of 5.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
