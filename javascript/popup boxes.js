c26e899d50d09a1729313fc49634e81ecb7ae0c1
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2024-02-22 15:20:30 +0530

    Update popup boxes.js

--- javascript/popup boxes.js
+++ javascript/popup boxes.js
@@ -4,3 +4,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+<style>
+        *{
+            margin:10px;
+            padding :5px;
+            
+        }
+        body{
+            background-color: rgb(30, 2, 11);
+        }
+        div{
+            background-color: rgb(238, 212, 225);
+            border:5px double rgb(12, 2, 5);
+            border-radius: 100%;
+            text-align: center;
+            justify-content: center;
+            width:900px;
+            height:fit-content;
+            margin-top:15%;
+            margin-left: 15%;
+            box-shadow:10px 10px 8px rgb(208, 205, 205);
+        }
+h1{
+    font-size: 50px;
+    color:rgb(61, 6, 17);
+    font-weight: 900;
+    text-decoration:underline ;
+}
+p{
+    font-size: 30px;
+    color:rgb(67, 14, 49);
+    font-weight: 300;
+}
+button{
+box-shadow: 5px 5px 5px black;
+background-color: rgb(250, 206, 148);
+cursor:pointer;
+transition:0.6s;
+}
+button:hover{
+    width: 135px;
+    font-weight: 500;
+    font-size: 16px;
+}
+#div2{
+            background-color: rgb(238, 212, 225);
+            border:5px double rgb(12, 2, 5);
+            border-radius: 100%;
+            text-align: center;
+            justify-content: center;
+            width:900px;
+            height:fit-content;
+            margin-top:15%;
+            margin-left: 15%;
+            box-shadow:10px 10px 8px rgb(208, 205, 205);
+        }
+
+    </style>
+</head>
+<body>
